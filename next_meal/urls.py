12050ac7a0912964714499e12e7fe16c9c6a75f1
XingKong <rxy19961107@gmail.com>
2022-10-29 18:53:56 +0800

    1

--- next_meal/urls.py
+++ next_meal/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('next_meal', views.next_meal),
+    path('', views.next_meal),
+    
 ]
--- next_meal/views.py
+++ next_meal/views.py
@@ -21,7 +21,20 @@ def dtf(data_list):
 def next_meal(request):
     """今天吃什么"""
     menus_df = dtf(menus.objects.all().values())
+    print("menus_df: \n", menus_df)
+
+    next_meal = menus_df.loc[0, 'menu']
 
     # run_log_df = dtf(run_log.objects.all().values())
 
-    return render(request, "powder_tscbys.html", locals())
+    return render(request, "next_meal.html", locals())
+
+"""
+    q2_text = request.POST.get('q1_text', '默认查询')
+    q2_mode = request.POST.get('q1_mode', '默认方式')
+
+    model = models_dic[q2_mode]
+    datalist = model(q2_text)
+
+    return render(request, "Vector_space/home.html", {"data":datalist})
+"""
