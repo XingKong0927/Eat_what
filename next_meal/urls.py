9c1991bcc99e2ef9f0677e41aff07ab0d0e89364
XingKong <rxy19961107@gmail.com>
2022-11-01 10:50:32 +0800

    完成美食录入功能
    next：运行日志

--- next_meal/urls.py
+++ next_meal/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.next_meal0),
-    path('run', views.next_meal),
+    path('home', views.next_meal),
     path('enter', views.enter_meal),
 ]
--- next_meal/views.py
+++ next_meal/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from eat_what.models import menus, run_log
 import pandas as pd
 
+from django.contrib import messages
+
 def dtf(data_list):
     """数据库数据转为dataframe，decimal转float
     """
@@ -22,59 +24,58 @@ def random_selection(a):
     b = randint(0,a)
     return b
 
-def next_meal0(request):
-    """今天吃什么"""
-    next_meal = '猜猜今天吃什么？'
-    next_meal_picture = 0
-    return render(request, "next_meal.html", locals())
-
 def next_meal(request):
     """今天吃什么"""
-    menus_df = dtf(menus.objects.all().values())
-    random_selection_index = random_selection(len(menus_df)-1)      # 随机选择index
-    print("random_selection_index: \n", random_selection_index)
+    if request.method == 'GET':
+        next_meal = '猜猜今天吃什么？'
+        next_meal_picture = 0
+        return render(request, "next_meal.html", locals())
+    else:
+        menus_df = dtf(menus.objects.all().values())
+        random_selection_index = random_selection(len(menus_df)-1)      # 随机选择index
 
-    next_meal = menus_df.loc[random_selection_index, 'menu']
-    next_meal_picture = "{}".format(menus_df.loc[random_selection_index, 'picture'])
-    next_meal_place = menus_df.loc[random_selection_index, 'place']
-    next_meal_price = menus_df.loc[random_selection_index, 'price']
-    next_meal_upload_time = menus_df.loc[random_selection_index, 'upload_time']
-    next_meal_upload_user = menus_df.loc[random_selection_index, 'upload_user']
+        next_meal = menus_df.loc[random_selection_index, 'menu']
+        next_meal_picture = "{}".format(menus_df.loc[random_selection_index, 'picture'])
+        next_meal_place = menus_df.loc[random_selection_index, 'place']
+        next_meal_price = menus_df.loc[random_selection_index, 'price']
+        next_meal_upload_time = menus_df.loc[random_selection_index, 'upload_time']
+        next_meal_upload_user = menus_df.loc[random_selection_index, 'upload_user']
 
-    # run_log_df = dtf(run_log.objects.all().values())
+        # run_log_df = dtf(run_log.objects.all().values())
 
-    return render(request, "next_meal.html", locals())
+        return render(request, "next_meal.html", locals())
 
 def enter_meal(request):
     """食物录入"""
     if request.method == 'GET':
         return render(request, "enter_meal.html")
     else:
-        # 获取一个文件管理器对象
-        file = request.FILES['pic']
-
-        # q2_text = request.POST.get('q1_text', '默认查询')
         # 保存文件
         menu_name = request.POST.get('menu_name')
-        # 将要保存的地址和文件名称
-        where = 'eat_what/static/Picture/{}.jpg'.format(menu_name)
-        # 分块保存image
-        content = file.chunks()
-        with open(where, 'wb') as f:
-            for i in content:
-                f.write(i)
-        print("menu_name: ", menu_name)
+        menus_df = dtf(menus.objects.all().values())
+        menus_list = list(menus_df['menu'])
+        # print("menus_list: \n", menus_list)
 
-        # # 上传文件名称到数据库
-        # menus.objects.filter(name='trent').update(avatar=menu_name)
-        # 返回的httpresponse
+        if(menu_name in menus_list):                    # 查重：名称为字符串
+            messages.error(request, '美食【{}】已存在，录入失败'.format(menu_name))
+        elif(menu_name == i for i in menus_list):       # 查重：名称为数字
+            messages.error(request, '美食【{}】已存在，录入失败'.format(menu_name))
+        else:
+            login_user = str(request.user)
+            
+            file = request.FILES['pic']     # 获取一个文件管理器对象
+            # 将要保存的地址和文件名称
+            where = 'eat_what/static/Picture/{}.jpg'.format(menu_name)
+            # 分块保存image
+            content = file.chunks()
+            with open(where, 'wb') as f:
+                for i in content:
+                    f.write(i)
+            menu_place = str(request.POST.get('menu_place'))
+            menu_price = request.POST.get('menu_price')
+
+            menus.objects.create(menu=menu_name, place=menu_place, upload_user=login_user, picture=menu_name, price=menu_price)       # 录入到数据库
+            messages.success(request, '美食【{}】录入成功'.format(menu_name))
+        menus_df = dtf(menus.objects.all().values())
+        # print("menus_df: \n", menus_df)
         return render(request, "enter_meal.html")
-
-"""
-    q2_mode = request.POST.get('q1_mode', '默认方式')
-
-    model = models_dic[q2_mode]
-    datalist = model(q2_text)
-
-    return render(request, "Vector_space/home.html", {"data":datalist})
-"""
