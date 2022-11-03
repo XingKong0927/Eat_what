3b7ce36c33d7eeef271680af46b4423182e92bfe
XingKong <rxy19961107@gmail.com>
2022-11-03 11:34:41 +0800

    完善选择地点功能
    next：运行日志

--- next_meal/views.py
+++ next_meal/views.py
@@ -10,9 +10,7 @@ def dtf(data_list):
     for dic in data_list:
         for i in dic:
             try:
-                if  i == 'id':
-                    pass
-                else:
+                if(i == 'price'):
                     dic[i] = float(dic[i])
             except:pass
     df = pd.DataFrame(list(data_list))    #转为dataframe
@@ -26,16 +24,18 @@ def random_selection(a):
 
 def next_meal(request):
     """今天吃什么"""
-    menus_df = dtf(menus.objects.all().values())
+    menus_df0 = dtf(menus.objects.all().values())
     if request.method == 'GET':
         next_meal = '猜猜今天吃什么？'
         next_meal_picture = 0
+        dining_place_list = list(menus_df0['place'])     # 就餐地点罗列
     else:
         dining_places = request.POST.getlist('dining_places')
-        menus_df = dtf(menus.objects.all().values())
-
-        menus_df = menus_df.loc[menus_df["place"]==dining_places]
-        print("menus_df: ", menus_df)
+        if dining_places:       # 选择了地点
+            menus_df = menus_df0[menus_df0["place"].isin(dining_places)]        # 根据list筛选数据
+            menus_df = menus_df.reset_index(drop=True)                          # 重置索引
+        else:                   # 没有选择地点
+            menus_df = menus_df0
         random_selection_index = random_selection(len(menus_df)-1)      # 随机选择index
 
         next_meal = menus_df.loc[random_selection_index, 'menu']
@@ -44,10 +44,10 @@ def next_meal(request):
         next_meal_price = menus_df.loc[random_selection_index, 'price']
         next_meal_upload_time = menus_df.loc[random_selection_index, 'upload_time']
         next_meal_upload_user = menus_df.loc[random_selection_index, 'upload_user']
+        dining_place_list = list(menus_df0['place'])     # 就餐地点罗列
 
         # run_log_df = dtf(run_log.objects.all().values())
-
-    dining_place_list = list(menus_df['place'])     # 就餐地点罗列
+    
     return render(request, "next_meal.html", locals())
 
 def enter_meal(request):
@@ -56,19 +56,16 @@ def enter_meal(request):
         return render(request, "enter_meal.html")
     else:
         # 保存文件
-        menu_name = request.POST.get('menu_name')
+        menu_name = str(request.POST.get('menu_name'))
         menus_df = dtf(menus.objects.all().values())
         menus_list = list(menus_df['menu'])
-        # print("menus_list: \n", menus_list)
 
-        if(menu_name in menus_list):                    # 查重：名称为字符串
-            messages.error(request, '美食【{}】已存在，录入失败'.format(menu_name))
-        if(True in [menu_name == i for i in menus_list]):       # 查重：名称为数字
+        if(menu_name in menus_list):                    # 查重
             messages.error(request, '美食【{}】已存在，录入失败'.format(menu_name))
         else:
             login_user = str(request.user)
             
-            file = request.FILES['pic']     # 获取一个文件管理器对象
+            file = request.FILES['picpath']     # 获取一个文件管理器对象
             # 将要保存的地址和文件名称
             where = 'eat_what/static/Picture/{}.jpg'.format(menu_name)
             # 分块保存image
@@ -82,5 +79,5 @@ def enter_meal(request):
             menus.objects.create(menu=menu_name, place=menu_place, upload_user=login_user, picture=menu_name, price=menu_price)       # 录入到数据库
             messages.success(request, '美食【{}】录入成功'.format(menu_name))
         menus_df = dtf(menus.objects.all().values())
-        print("menus_df: \n", menus_df)
+        # print("menus_df: \n", menus_df)
         return render(request, "enter_meal.html")
